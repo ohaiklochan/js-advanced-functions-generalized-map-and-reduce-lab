0be3844a5c9450b94c43b83926878d926ab7d2bd
ohaiklochan <caforonda103@yahoo.com>
2021-03-29 16:18:40 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Add your functions here
+
+function map(array, fn) {
+    return array.map(fn)
+};
+
+function reduce(array, fn, startingPoint) {
+    if(!!startingPoint == false) {
+        startingPoint = array.shift();
+    }
+    return array.reduce(fn, startingPoint);
+};
